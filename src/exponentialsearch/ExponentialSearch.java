698d4dedd664c1e3fde47e730d28758cd9cd50ef
Asim Habib <de.asimhabib@gmail.com>
2020-10-15 22:28:37 +0500

    Wrote body of both exponSearch and binarySearch

--- src/exponentialsearch/ExponentialSearch.java
+++ src/exponentialsearch/ExponentialSearch.java
@@ -1,8 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package exponentialsearch;
 
 /**
@@ -10,12 +6,36 @@ package exponentialsearch;
  * @author asim
  */
 public class ExponentialSearch {
-
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
+    
+    public int exponSearch(int array[], int value)
+    {
+        
+        int i=1;
+        
+        while (i<array.length && array[i]<value)
+            i*=2;
+        return binarySearch(array,i/2,Integer.min(i, array.length-1),value );
+        
+    }
+    
+    
+    public int binarySearch(int array[], int start , int end , int value)
+    {
+        int foundIndex=-1;
+        while (start<=end) 
+        {
+            int mid=(start+end)/2;
+            if (array[mid]==value) 
+            {
+               foundIndex=mid;
+               break;
+            }
+            else if(value<array[mid])
+                end=mid-1;
+            else
+                start=mid+1;
+        }
+        return foundIndex;
     }
     
 }
